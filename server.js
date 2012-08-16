82d9c254669533bd0ec5f7e9b6143a9ba02f1bbf
Roman Udovichenko <mailromka@gmail.com>
2012-08-16 20:42:10 +0300

    use another example

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-var io = require('socket.io').listen(process.env.PORT || 8080);
-
+var port=process.env.PORT || 3000;
+var http=require('http');
+var app=http.createServer(function(req,res){
+    res.write("server listening to port:"+port);
+    res.end();
+}).listen(port);
+socket=require("socket.io");
+io=socket.listen(app);
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
 });
-
-io.sockets.on('connection', function (socket) {
-  socket.on('set nickname', function (name) {
-    socket.set('nickname', name, function () {
-      socket.emit('ready');
-    });
-  });
-
-  socket.on('msg', function () {
-    socket.get('nickname', function (err, name) {
-      console.log('Chat message by ', name);
-    });
-  });
+io.sockets.on("connection",function(socket){
+    console.log("new connection");
+    socket.on("eventA",function(data){
+        io.sockets.emit("eventB",data);
+    }); 
 });
