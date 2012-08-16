3434efd2e74c27735dcda0fed9fa1024ec5b73c2
Roman Udovichenko <mailromka@gmail.com>
2012-08-16 20:25:23 +0300

    example socket.io

---
+++ server.js
@@ -0,0 +1,20 @@
+var io = require('socket.io').listen(8080);
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('set nickname', function (name) {
+    socket.set('nickname', name, function () {
+      socket.emit('ready');
+    });
+  });
+
+  socket.on('msg', function () {
+    socket.get('nickname', function (err, name) {
+      console.log('Chat message by ', name);
+    });
+  });
+});
